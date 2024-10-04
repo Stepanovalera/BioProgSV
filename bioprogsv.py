f8ad3f410c3883b9423c647ce1cb4006f056ca8c
Stepanova Valeriya <ukrainskaya49@gmail.com>
2024-10-04 11:06:01 +0300

    Добавлен import, dockstring

--- bioprogsv.py
+++ bioprogsv.py
@@ -1,8 +1,20 @@
 
-import sv_rdrt
-import
+from addscript.sv_rdrt_functions import transcribe, reverse, complement, reverse_complement
+from addscript.sv_check import is_none
+from addscript.fastq_qc_function import fast_qc
+
 
 def run_dna_rna_tools(*args):  
+    """
+    Parameters:
+    *args (list): list of seqs(list) and action(str)
+
+    Returns:
+    result (list or str): A list of DNA/RNA sequences or single DNA/RNA sequence
+
+    Module:
+    is_none - checking is value None or not
+    """
     results = []
     *seqs, action = args
     for seq in seqs:
@@ -25,13 +37,22 @@ def run_dna_rna_tools(*args):
 
 
 def filter_fastq(seqs, gc_bounds=(0, 100), length_bounds=(0, 2**32), quality_threshold=0):
+    """
+    Parameters:
+    - seqs dict[str, tuple[str, str]: A dictionary where each key is a sequence name and each value
+      is a tuple containing the DNA sequence (str) and its quality string (str).
+    - gc_bounds(tuple or float):
+    - length_bounds(tuple):
+    - quality_threshold(int): 
+
+    Returns:
+    - filtered_fastq(dict): A dictionary of selected seqs 
+    """
     filtered_fastq = {}
-    filtered_data = fast_gc(seqs)
+    filtered_data = fast_qc(seqs)
     for sequence_name, (gc, length, quality) in filtered_data.items():
         if (gc_bounds[0] <= gc <= gc_bounds[1]) and \
            (length_bounds[0] <= length <= length_bounds[1]) and \
            (quality > quality_threshold):
             filtered_fastq[sequence_name] = seqs[sequence_name]
-        return filtered_fastq
-
-
+    return filtered_fastq
